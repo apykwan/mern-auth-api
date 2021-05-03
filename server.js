c5593b823e8d8478351c56fbe6dca3d32cc57a11
apykwan <apykwan@hotmail.com>
2021-05-03 10:56:53 -0700

    some change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/api/', authRoutes);
 app.use('/api/user', userRoutes);
 
-const port = process.env.PORT || 8003;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`)
